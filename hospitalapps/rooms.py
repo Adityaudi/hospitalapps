4275d4c25e111b814c79906bd8cedc85be48f51c
Aditya Udi Anggoro <69835381+Adityaudi@users.noreply.github.com>
2021-12-02 20:59:07 +0700

    git init

---
+++ hospitalapps/rooms.py
@@ -0,0 +1,320 @@
+import os
+import data
+
+def inap_president(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap President Suite
+    Rp. {Hcoret},-   Rp. {Hasli},- \n
+    Fasilitas:
+        - AC 
+        - 1 Bed 
+        - 1 Kamar mandi dengan air panas & dingin 
+        - Bedside cabinet 
+        - Kulkas
+        - Kursi tunggu 
+        - Overbed Table 
+        - Meja TV 
+        - Almari pakaian 
+        - Dispenser 
+        - Meja 
+        - Sofabed 
+        - Smart TV 72" 
+        - Bed Penunggu 
+        - Telepon 
+        - Ruang keluarga 
+        - 1 set meja makan \n
+    Photo: {photo}
+    ''')
+def inap_VVIP_A(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap VVIP A
+    Rp. {Hcoret},-   Rp. {Hasli},- \n
+    Fasilitas:
+        - AC 
+        - 1 Bed 
+        - 1 Kamar mandi dengan air panas & dingin 
+        - Bedside cabinet 
+        - Kulkas
+        - Kursi tunggu
+        - Overbed Table 
+        - Meja TV 
+        - Almari pakaian 
+        - Dispenser 
+        - Meja 
+        - Sofa 
+        - LED TV 47" 
+        - Bed Penunggu 
+        - Telepon 
+        - Ruang keluarga 
+        - 1 set meja makan 
+        - Kitchen Set  \n
+    Photo: {photo}
+    ''')
+
+def inap_VVIP_B(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap VVIP B
+    Rp. {Hcoret},-   Rp. {Hasli},- \n
+    Fasilitas:
+        - AC 
+        - 1 Bed 
+        - 1 Kamar mandi dengan air panas & dingin 
+        - Bedside cabinet 
+        - Kulkas
+        - Kursi tunggu 
+        - Overbed Table 
+        - Meja TV 
+        - Almari pakaian 
+        - Dispenser 
+        - Meja 
+        - Sofabed 
+        - LCD 32" 
+        - Bed Penunggu 
+        - Telepon 
+        - Ruang keluarga 
+        - 1 set meja makan
+     \n
+    Photo: {photo}
+    ''')
+def inap_VIP_Lily(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap VIP Lily
+    Rp. {Hcoret},-   Rp. {Hasli},- \n \n
+    Fasilitas:
+        - AC 
+        - 1 Bed 
+        - 1 Kamar mandi dengan air panas & dingin 
+        - Bedside cabinet 
+        - Kulkas
+        - Kursi tunggu 
+        - Overbed Table 
+        - Meja TV 
+        - Almari pakaian 
+        - Dispenser 
+        - Meja 
+        - Sofa
+        - LED TV 43" 
+        - Bed Penunggu 
+        - Telepon 
+        - Mini Bar \n
+    Photo: {photo}
+    ''')
+def inap_VIP_A(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap VIP A
+    Rp. {Hcoret},-   Rp. {Hasli},- \n
+    Fasilitas:
+        - AC 
+        - 1 Bed 
+        - 1 Kamar mandi dengan air panas & dingin 
+        - Bedside cabinet 
+        - Kulkas
+        - Kursi tunggu 
+        - Overbed Table 
+        - Meja TV 
+        - Almari pakaian 
+        - Dispenser 
+        - Meja 
+        - Sofa 
+        - LED TV 43" 
+        - Bed Penunggu 
+        - Telepon 
+        - Mini Bar  \n
+    Photo: {photo}
+    ''')
+def inap_VIP_B(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap VIP B
+    Rp. {Hcoret},-   Rp. {Hasli},- \n
+    Fasilitas:
+        - AC 
+        - 1 Bed 
+        - 1 Kamar mandi dengan air panas & dingin 
+        - Bed penunggu
+        - Kulkas
+        - Bedside Cabinet 
+        - Kursi tunggu 
+        - 1 Dispenser 
+        - 1 buah LCD 32" 
+        - Lemari Pakaian 
+        - Meja 
+        - Sofa 
+        - Telepon  \n
+    Photo: {photo}
+    ''')
+
+def inap_VIP_C(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap VIP C
+    Rp. {Hcoret},-   Rp. {Hasli},- \n
+    Fasilitas:
+        - AC 
+        - 1 Kamar mandi dengan air panas dan dingin 
+        - 1 set Sofa+meja 
+        - 1 Bed 
+        - Bedside Cabinet 
+        - Bed Penunggu 
+        - Kursi penunggu pasien 
+        - TV 21 Inch  \n
+    Photo: {photo}
+    ''')
+def inap_KELAS_A(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap Comalia / Kelas A
+    Rp. {Hcoret},-   Rp. {Hasli},- \n
+    Fasilitas:
+        - AC 
+        - 2 Bed 
+        - 1 Kamar mandi dengan air panas & dingin 
+        - 2 buah bedside cabinet 
+        - Overbed table 
+        - 2 kursi penunggu
+        - 2 buah TV  \n
+    Photo: {photo}
+    ''')
+def inap_KELAS_B(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap Bougenville / Kelas B
+    Rp. {Hcoret},-   Rp. {Hasli},- \n
+    Fasilitas:
+        - AC 
+        - 4 Bed 
+        - 2 Kamar mandi dengan air panas & dingin 
+        - 4 buah bedside cabinet 
+        - overbed table 
+        - 4 kursi penunggu  \n
+    Photo: {photo}
+    ''')
+def inap_KELAS_C(harga, diskon, photo):
+    Hcoret = str(harga)
+    Hcoret = ''.join([u'\u0336{}'.format(c) for c in f'{Hcoret}'])
+    Hdiskon = harga * diskon / 100
+    Hasli = harga - Hdiskon
+    Hasli = int(Hasli)
+    os.system("clear")
+    data.init()
+    data.rawatinap.append(Hasli)
+    print(
+    f'''
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                SANGKURIANG HOSPITAL
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- \n\n
+    Kamar Inap Gardenia / Kelas C
+    Rp. {Hcoret},-   Rp. {Hasli},- \n
+    Fasilitas:
+        - AC 
+        - 6 Bed 
+        - 2 Kamar mandi dengan air panas & dingin 
+        - 6 buah bedside cabinet 
+        - Overbed table 
+        - 6 Kursi penunggu  \n
+    Photo: {photo}
+    ''')
