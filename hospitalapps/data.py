0fb6774803c7364b72bf108908893699829f72fb
Aditya Udi Anggoro <69835381+Adityaudi@users.noreply.github.com>
2021-12-02 20:59:53 +0700

    Delete hospitalapps directory

--- hospitalapps/data.py
+++
@@ -1,9 +0,0 @@
-def init():
-    global BiodataPasien
-    global rawatinap
-    global NoKartu
-    global Pakaibpjs
-    BiodataPasien = [] # Biodata pasien data
-    rawatinap = [] # Nama, Harga Kamar
-    NoKartu = [] # ATM, BPJS/KIS
-    Pakaibpjs = []
